966dc4437f43e2ef043256f7b13c7f2b2129f157
Julia Pereira <vital.pereira.julia@gmail.com>
2025-01-13 08:43:42 -0300

    componente de Loading

--- igniteteams/app/(tabs)/index.tsx
+++ igniteteams/app/(tabs)/index.tsx
@@ -2,7 +2,7 @@ import Groups from "@/src/screens/Groups";
 import theme from "@/src/theme";
 import { ThemeProvider } from 'styled-components'
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto'
-import { ActivityIndicator } from "react-native";
+import { Loading } from "@/src/components/Loading";
 
 export default function App() {
 const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold});
@@ -11,7 +11,7 @@ const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold});
 
   return (
     <ThemeProvider theme={theme}>
-     {fontsLoaded ? <Groups /> : <ActivityIndicator />}
+     {fontsLoaded ? <Groups /> : <Loading />}
     </ThemeProvider>
     
   );
