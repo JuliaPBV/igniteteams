6502fdf336bcdaf36a37f6de71820b57701601c9
Julia Pereira <vital.pereira.julia@gmail.com>
2025-01-13 13:59:24 -0300

    listando grupos

--- igniteteams/src/screens/Groups/index.tsx
+++ igniteteams/src/screens/Groups/index.tsx
@@ -1,10 +1,15 @@
 import { Container } from "./styles";
 
+import { useState } from "react";
+import { FlatList } from "react-native";
+
 import { GroupCard } from "@/src/components/GroupCard";
 import { Highlight } from "@/src/components/Highlight";
 import { Header } from "@/src/components/Header";
 
 export default function Groups() {
+    const [groups, setGroups] = useState<string[]>(['Galera da Rocket']);
+
     return (
         <Container>
             <Header />
@@ -14,7 +19,15 @@ export default function Groups() {
             subtitle="jogue com sua turma"
             />
 
-            <GroupCard title="Galera do Ignite" />
+            <FlatList 
+            data={groups}
+            keyExtractor={item => item}
+            renderItem={({ item }) => (
+            <GroupCard 
+            title={item} 
+            />)}
+            />
+
         </Container>
     );
 }
