238552b3c0b9d2100ca56a9e7a1954a8b6f7cb7d
Julia Pereira <vital.pereira.julia@gmail.com>
2025-01-13 08:50:32 -0300

    statusBar

--- igniteteams/app/(tabs)/index.tsx
+++ igniteteams/app/(tabs)/index.tsx
@@ -3,6 +3,7 @@ import theme from "@/src/theme";
 import { ThemeProvider } from 'styled-components'
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto'
 import { Loading } from "@/src/components/Loading";
+import { StatusBar } from "react-native";
 
 export default function App() {
 const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold});
@@ -11,6 +12,11 @@ const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold});
 
   return (
     <ThemeProvider theme={theme}>
+      <StatusBar 
+      barStyle="light-content"
+      backgroundColor="trasnparent"
+      translucent
+      />
      {fontsLoaded ? <Groups /> : <Loading />}
     </ThemeProvider>
     
