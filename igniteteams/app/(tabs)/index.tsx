c57de0c88f1765b0b40c676b70fb17110e1347e0
Julia Pereira <vital.pereira.julia@gmail.com>
2025-01-13 08:23:37 -0300

    fontes personalizadas

--- igniteteams/app/(tabs)/index.tsx
+++ igniteteams/app/(tabs)/index.tsx
@@ -1,11 +1,17 @@
 import Groups from "@/src/screens/Groups";
 import theme from "@/src/theme";
 import { ThemeProvider } from 'styled-components'
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto'
+import { ActivityIndicator } from "react-native";
 
 export default function App() {
+const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold});
+
+
+
   return (
     <ThemeProvider theme={theme}>
-      <Groups />
+     {fontsLoaded ? <Groups /> : <ActivityIndicator />}
     </ThemeProvider>
     
   );
