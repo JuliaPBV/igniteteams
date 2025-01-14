dd9d8e95feceba0a1bb4996b2def8891cd05982b
Julia Pereira <vital.pereira.julia@gmail.com>
2025-01-14 19:27:40 -0300

    tipando as rotas

--- igniteteams/app/(tabs)/index.tsx
+++ igniteteams/app/(tabs)/index.tsx
@@ -1,24 +1,25 @@
-import {Players} from "@/src/screens/Players";
+import { Routes } from "@/src/routes";
 import theme from "@/src/theme";
-import { ThemeProvider } from 'styled-components'
-import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto'
+import { ThemeProvider } from "styled-components";
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from "@expo-google-fonts/roboto";
 import { Loading } from "@/src/components/Loading";
 import { StatusBar } from "react-native";
 
 export default function App() {
-const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold});
-
-
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
 
   return (
     <ThemeProvider theme={theme}>
-      <StatusBar 
-      barStyle="light-content"
-      backgroundColor="trasnparent"
-      translucent
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="trasnparent"
+        translucent
       />
-     {fontsLoaded ? <Players /> : <Loading />}
+      {fontsLoaded ? <Routes /> : <Loading />}
     </ThemeProvider>
-    
   );
 }
--- igniteteams/src/screens/Groups/index.tsx
+++ igniteteams/src/screens/Groups/index.tsx
@@ -8,40 +8,30 @@ import { Highlight } from "@/src/components/Highlight";
 import { Header } from "@/src/components/Header";
 import { ListEmpty } from "@/src/components/ListEmpty";
 import { Button } from "@/src/components/Button";
-
+import { useNavigation } from "@react-navigation/native";
 
 export default function Groups() {
-    const [groups, setGroups] = useState<string[]>([]);
+  const [groups, setGroups] = useState<string[]>([]);
 
-    return (
-        <Container>
-            <Header />
+  const navigation = useNavigation();
 
-            <Highlight 
-            title="Turmas"
-            subtitle="jogue com sua turma"
-            />
+  return (
+    <Container>
+      <Header />
 
-            <FlatList 
-            data={groups}
-            keyExtractor={item => item}
-            renderItem={({ item }) => (
-            <GroupCard 
-            title={item} 
-            />
-        )}
+      <Highlight title="Turmas" subtitle="jogue com sua turma" />
+
+      <FlatList
+        data={groups}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => <GroupCard title={item} />}
         contentContainerStyle={groups.length === 0 && { flex: 1 }}
         ListEmptyComponent={() => (
-        <ListEmpty 
-        message="Que tal cadastrar a primeira turma?" 
-        />
-       )}
-     />
+          <ListEmpty message="Que tal cadastrar a primeira turma?" />
+        )}
+      />
 
-       <Button 
-       title="Criar nova turma"
-       />
-        </Container>
-    );
+      <Button title="Criar nova turma" onPress={handleNewGroup} />
+    </Container>
+  );
 }
-
