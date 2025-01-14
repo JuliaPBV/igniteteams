fd044a1f14485edeacfdcad989d5ba7a707eb57d
Julia Pereira <vital.pereira.julia@gmail.com>
2025-01-14 08:49:00 -0300

    deixando o ícone dinâmico

--- igniteteams/src/screens/Players/index.tsx
+++ igniteteams/src/screens/Players/index.tsx
@@ -1,6 +1,8 @@
 import { Header } from '@/src/components/Header';
-import { Container } from './styles';
+import { ButtonIcon } from '@/src/components/ButtonIcon';
+import { Container, Form } from './styles';
 import { Highlight } from '@/src/components/Highlight';
+import { Input } from '@/src/components/Input';
 
 export function Players() {
     return (
@@ -11,6 +13,17 @@ export function Players() {
             title="Nome da turma"
             subtitle="adicione o pessoal e separe os times"
             />
+
+        <Form>
+            <Input 
+            placeholder="Nome da pessoa"
+            autoCorrect={false}
+            />
+
+            <ButtonIcon 
+            icon='add'
+            />
+            </Form>
         </Container>
     )
 }
