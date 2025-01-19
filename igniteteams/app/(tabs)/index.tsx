1cd3788ac5403491071f4704e318073e7fe846ed
Julia Pereira <vital.pereira.julia@gmail.com>
2025-01-19 11:56:30 -0300

    executando projeto

--- igniteteams/app/(tabs)/index.tsx
+++ igniteteams/app/(tabs)/index.tsx
@@ -1,25 +1,5 @@
-import { Routes } from "@/src/routes";
-import theme from "@/src/theme";
-import { ThemeProvider } from "styled-components";
-import {
-  useFonts,
-  Roboto_400Regular,
-  Roboto_700Bold,
-} from "@expo-google-fonts/roboto";
-import { Loading } from "@/src/components/Loading";
-import { StatusBar } from "react-native";
+import Groups from "@/src/screens/Groups";
 
 export default function App() {
-  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
-
-  return (
-    <ThemeProvider theme={theme}>
-      <StatusBar
-        barStyle="light-content"
-        backgroundColor="trasnparent"
-        translucent
-      />
-      {fontsLoaded ? <Routes /> : <Loading />}
-    </ThemeProvider>
-  );
+  return <Groups />;
 }
--- igniteteams/src/screens/Groups/index.tsx
+++ igniteteams/src/screens/Groups/index.tsx
@@ -1,63 +1,18 @@
-import { Container } from "./styles";
-
-import { useState, useCallback } from "react";
-import { FlatList } from "react-native";
-
-import { GroupCard } from "@/src/components/GroupCard";
-import { Highlight } from "@/src/components/Highlight";
-import { Header } from "@/src/components/Header";
-import { ListEmpty } from "@/src/components/ListEmpty";
-import { Button } from "@/src/components/Button";
-import { useNavigation, useFocusEffect } from "@react-navigation/native";
-import { groupsGetAll } from "@/src/storage/group/groupsGetAll";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function Groups() {
-  const [groups, setGroups] = useState<string[]>([]);
-
-  const navigation = useNavigation();
-
-  function handleNewGroup() {
-    navigation.navigate("new");
-  }
-
-  async function fetchGroups() {
-    try {
-      const data = await groupsGetAll();
-      setGroups(data);
-    } catch (error) {
-      console.log(error);
-    }
-  }
-
-  function handleOpenGroup(group: string) {
-    navigation.navigate("players", { group });
-  }
-
-  useFocusEffect(
-    useCallback(() => {
-      fetchGroups();
-    }, [])
-  );
-
   return (
-    <Container>
-      <Header />
-
-      <Highlight title="Turmas" subtitle="jogue com sua turma" />
-
-      <FlatList
-        data={groups}
-        keyExtractor={(item) => item}
-        renderItem={({ item }) => (
-          <GroupCard title={item} onPress={() => handleOpenGroup(item)} />
-        )}
-        contentContainerStyle={groups.length === 0 && { flex: 1 }}
-        ListEmptyComponent={() => (
-          <ListEmpty message="Que tal cadastrar a primeira turma?" />
-        )}
-      />
-
-      <Button title="Criar nova turma" onPress={handleNewGroup} />
-    </Container>
+    <View style={styles.container}>
+      <Text>Groups</Text>
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#FFF",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
