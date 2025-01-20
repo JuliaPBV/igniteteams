b5473d4349c927013651afbbc01403f80e281e57
Julia Pereira <vital.pereira.julia@gmail.com>
2025-01-20 10:03:33 -0300

    componente ListEmpty

--- igniteteams/src/screens/Groups/index.tsx
+++ igniteteams/src/screens/Groups/index.tsx
@@ -1,16 +1,29 @@
-import { Header } from "@/src/components/Header";
-import { Container } from "./styles";
 import { Highlight } from "@/src/components/Highlight";
+import { Container } from "./styles";
+import { Header } from "@/src/components/Header";
 import { GroupCard } from "@/src/components/GroupCard";
+import { useState } from "react";
+import { FlatList } from "react-native";
+import { ListEmpty } from "@/src/components/ListEmpty";
 
 export default function Groups() {
+  const [groups, setGroups] = useState<string[]>([]);
+
   return (
     <Container>
       <Header />
 
-      <Highlight title="Turmas" subtitle="Jogue com a sua turma" />
+      <Highlight title="Turmas" subtitle="jogue com sua turma" />
 
-      <GroupCard title="Galera do Ignite" />
+      <FlatList
+        data={groups}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => <GroupCard title={item} />}
+        contentContainerStyle={groups.length === 0 && { flex: 1 }}
+        ListEmptyComponent={() => (
+          <ListEmpty message="Que tal cadastrar a primeira turma?" />
+        )}
+      />
     </Container>
   );
 }
