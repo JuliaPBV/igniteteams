ab80338ceffda121ff50d03d4068652c7ccb445f
Julia Pereira <vital.pereira.julia@gmail.com>
2025-01-19 22:34:22 -0300

    criando o componente GroupCard

--- igniteteams/src/screens/Groups/index.tsx
+++ igniteteams/src/screens/Groups/index.tsx
@@ -1,12 +1,16 @@
 import { Header } from "@/src/components/Header";
 import { Container } from "./styles";
 import { Highlight } from "@/src/components/Highlight";
+import { GroupCard } from "@/src/components/GroupCard";
 
 export default function Groups() {
   return (
     <Container>
       <Header />
+
       <Highlight title="Turmas" subtitle="Jogue com a sua turma" />
+
+      <GroupCard title="Galera do Ignite" />
     </Container>
   );
 }
