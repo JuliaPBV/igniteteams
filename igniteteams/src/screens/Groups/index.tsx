c1b1a42c7a5b2e9d64b83e05f3dff0124dc2c6ab
Julia Pereira <vital.pereira.julia@gmail.com>
2025-01-19 22:22:22 -0300

    criando componente Highlight

--- igniteteams/src/screens/Groups/index.tsx
+++ igniteteams/src/screens/Groups/index.tsx
@@ -1,10 +1,12 @@
 import { Header } from "@/src/components/Header";
 import { Container } from "./styles";
+import { Highlight } from "@/src/components/Highlight";
 
 export default function Groups() {
   return (
     <Container>
       <Header />
+      <Highlight title="Turmas" subtitle="Jogue com a sua turma" />
     </Container>
   );
 }
